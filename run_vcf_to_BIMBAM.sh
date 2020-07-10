d75f0753f6284b9eeca047d3cfb192a6369bc8fa
Alan Michael Pittman <47422569+alanmichaelpittman100@users.noreply.github.com>
2020-07-10 16:32:26 +0100

    Update run_vcf_to_BIMBAM.sh

--- run_vcf_to_BIMBAM.sh
+++ run_vcf_to_BIMBAM.sh
@@ -1,5 +1,5 @@
-GEMMA="/homes/athosnew/Genetics_Centre_Bioinformatics/resourses/gemma/gemma-0.98.1-linux-static "
-inputFilesDir="/homes/athosnew/Genetics_Centre_Bioinformatics/incoming_data/DTR_Genotypes_1000G_V3P5_Collab_PublicID"
-qctool2="/homes/athosnew/Genetics_Centre_Bioinformatics/resourses/qctool_v2.0.1-Ubuntu14.04-x86_64/qctool"
+GEMMA="/resourses/gemma/gemma-0.98.1-linux-static "
+inputFilesDir="/incoming_data/DTR_Genotypes_1000G_V3P5_Collab_PublicID"
+qctool2="/qctool_v2.0.1-Ubuntu14.04-x86_64/qctool"
 
-/homes/athosnew/Genetics_Centre_Bioinformatics/resourses/qctool_v2.0.1-Ubuntu14.04-x86_64/qctool -g Merged.vcf.gz -ofiletype bimbam_dosage -vcf-genotype-field GP -og crypto_merged_dosage.bimbam
+/resourses/qctool_v2.0.1-Ubuntu14.04-x86_64/qctool -g Merged.vcf.gz -ofiletype bimbam_dosage -vcf-genotype-field GP -og crypto_merged_dosage.bimbam
